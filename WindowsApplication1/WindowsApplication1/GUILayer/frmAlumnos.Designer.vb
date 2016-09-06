cff2e257879a70c1926d4a9f46f9eb28d10cd4e3
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-06 15:51:27 -0300

    Agregue el formulario Alumno para que usemos de ejemplo

---
+++ WindowsApplication1/WindowsApplication1/GUILayer/frmAlumnos.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblMensajes = New System.Windows.Forms.Label()
+        Me.cmdBuscar = New System.Windows.Forms.Button()
+        Me.txtNombres = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtLegajo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cmdConsultar = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cmdSalir = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblMensajes)
+        Me.Panel1.Controls.Add(Me.cmdBuscar)
+        Me.Panel1.Controls.Add(Me.txtNombres)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.txtApellido)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.txtLegajo)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(32, 15)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(572, 458)
+        Me.Panel1.TabIndex = 29
+        '
+        'lblMensajes
+        '
+        Me.lblMensajes.AutoSize = True
+        Me.lblMensajes.Location = New System.Drawing.Point(381, 12)
+        Me.lblMensajes.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblMensajes.Name = "lblMensajes"
+        Me.lblMensajes.Size = New System.Drawing.Size(90, 17)
+        Me.lblMensajes.TabIndex = 27
+        Me.lblMensajes.Text = "[lblMensajes]"
+        '
+        'cmdBuscar
+        '
+        Me.cmdBuscar.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.cmdBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdBuscar.Location = New System.Drawing.Point(249, 12)
+        Me.cmdBuscar.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdBuscar.Name = "cmdBuscar"
+        Me.cmdBuscar.Size = New System.Drawing.Size(113, 28)
+        Me.cmdBuscar.TabIndex = 26
+        Me.cmdBuscar.Text = "Buscar"
+        Me.cmdBuscar.UseVisualStyleBackColor = False
+        '
+        'txtNombres
+        '
+        Me.txtNombres.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNombres.Location = New System.Drawing.Point(120, 76)
+        Me.txtNombres.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtNombres.Name = "txtNombres"
+        Me.txtNombres.Size = New System.Drawing.Size(389, 22)
+        Me.txtNombres.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 79)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(69, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Nombres:"
+        '
+        'txtApellido
+        '
+        Me.txtApellido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtApellido.Location = New System.Drawing.Point(120, 44)
+        Me.txtApellido.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(242, 22)
+        Me.txtApellido.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 47)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Apellido:"
+        '
+        'txtLegajo
+        '
+        Me.txtLegajo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtLegajo.Location = New System.Drawing.Point(120, 14)
+        Me.txtLegajo.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtLegajo.Name = "txtLegajo"
+        Me.txtLegajo.Size = New System.Drawing.Size(121, 22)
+        Me.txtLegajo.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 16)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Legajo:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.cmdConsultar)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Location = New System.Drawing.Point(629, 15)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(537, 458)
+        Me.GroupBox1.TabIndex = 30
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Lista de Alumnos"
+        '
+        'cmdConsultar
+        '
+        Me.cmdConsultar.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.cmdConsultar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdConsultar.Location = New System.Drawing.Point(597, 29)
+        Me.cmdConsultar.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdConsultar.Name = "cmdConsultar"
+        Me.cmdConsultar.Size = New System.Drawing.Size(167, 38)
+        Me.cmdConsultar.TabIndex = 17
+        Me.cmdConsultar.Text = "Consultar"
+        Me.cmdConsultar.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(52, 429)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 17)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Alumno:"
+        '
+        'cmdSalir
+        '
+        Me.cmdSalir.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.cmdSalir.Location = New System.Drawing.Point(1051, 481)
+        Me.cmdSalir.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmdSalir.Name = "cmdSalir"
+        Me.cmdSalir.Size = New System.Drawing.Size(107, 44)
+        Me.cmdSalir.TabIndex = 31
+        Me.cmdSalir.Text = "Salir"
+        Me.cmdSalir.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(119, 429)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(320, 22)
+        Me.TextBox1.TabIndex = 18
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(8, 29)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(521, 376)
+        Me.DataGridView1.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1210, 527)
+        Me.Controls.Add(Me.cmdSalir)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lblMensajes As System.Windows.Forms.Label
+    Friend WithEvents cmdBuscar As System.Windows.Forms.Button
+    Friend WithEvents txtNombres As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtApellido As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtLegajo As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cmdConsultar As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cmdSalir As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ WindowsApplication1/WindowsApplication1/GUILayer/frmAlumnos.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private listaCalificaciones As List(Of Calificaciones)
+
+    Private Sub cmdBuscar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdBuscar.Click
+        Try
+            lblMensajes.Text = String.Empty
+            Dim alumno = BDHelper.GetAlumno(txtLegajo.Text)
+            If (alumno IsNot Nothing) Then
+                txtNombres.Text = alumno.Nombre
+                txtApellido.Text = alumno.Apellido
+                cmdConsultar.Enabled = True
+            Else
+                cmdConsultar.Enabled = False
+                lblMensajes.Text = "El legajo no existe"
+            End If
+        Catch ex As Exception
+            lblMensajes.Text = ex.Message
+        End Try
+    End Sub
+
+    Private Sub cmdConsultar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdConsultar.Click
+        Dim fecha As DateTime
+        Try
+            'Limpia la ettiqueta de mensajes
+            lblMensajes.Text = String.Empty
+            'Carga en una lista de calificaciones la consulta hecha a la base de datos
+            Dim calificaciones As List(Of Calificaciones) = BDHelper.GetCalificaciones(txtLegajo.Text, cmbMaterias.SelectedValue)
+            If (calificaciones.Count = 0) Then
+                lblMensajes.Text = "No existen calificaciones"
+            End If
+            'Limpia la grilla de datos anteriores
+            dgvCalificaciones.Rows.Clear()
+            'Por cada elemento de la lista de calificaciones, agrega una fila en la grilla 
+            For Each calificacion As Calificaciones In calificaciones
+                dgvCalificaciones.Rows.Add((New Object() {calificacion.Fecha, calificacion.Nota, calificacion.NombreProfesor}))
+            Next
+
+        Catch ex As Exception
+            lblMensajes.Text = ex.Message
+        End Try
+    End Sub
+
+
+    Private Sub CargarCombo(ByRef combo As ComboBox, ByRef datos As Data.DataTable, ByVal pk As String, ByVal descripcion As String)
+        'combo.Items.Clear()
+        combo.DataSource = datos
+        combo.ValueMember = pk
+        combo.DisplayMember = descripcion
+
+    End Sub
+
+    Private Sub cmdSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSalir.Click
+        Me.Close()
+    End Sub
+
+    Private Sub dgvCalificaciones_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub cmdGuardar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Try
+            lblMensajes.Text = String.Empty
+            BDHelper.GuardarCalificaciones(listaCalificaciones)
+            lblMensajes.Text = "Las calificaciones se guardaron correctamente."
+        Catch ex As Exception
+            lblMensajes.Text = ex.Message
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            lblMensajes.Text = String.Empty
+
+            listaCalificaciones = New List(Of Calificaciones)()
+            cmdConsultar.Enabled = False
+            CargarCombo(cmbMaterias, BDHelper.GetMaterias(), "id_materia", "nombre_materia")
+        Catch ex As Exception
+            lblMensajes.Text = ex.Message
+        End Try
+    End Sub
+
+
+End Class
