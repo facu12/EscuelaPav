8ead37f7b71bf938ef2d4c40ec9f976acd54cc1f
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-06 18:13:29 -0300

    Arreglo de getAlumnos legajo a int

--- WindowsApplication1/WindowsApplication1/DataLayer/DAOs/AlumnoDAO.vb
+++ WindowsApplication1/WindowsApplication1/DataLayer/DAOs/AlumnoDAO.vb
@@ -11,9 +11,9 @@
         'Con la tabla devuelta por el Helper creamos un OBJETO Alumno a partir de los datos de fila de la tabla Alumnos
         For Each row As DataRow In BDHelper.getDBHelper().ConsultaSQL(strSQL).Rows
             oAlumno = New Alumno()
-            oAlumno.legajo = oTable.Rows(0).Item("legajo").ToString
+            oAlumno.legajo = Convert.ToInt32(oTable.Rows(0).Item("legajo").ToString)
             oAlumno.apellido = oTable.Rows(0).Item("apellido").ToString
-            oAlumno.nombre = Convert.ToInt32(oTable.Rows(0).Item("nombre").ToString)
+            oAlumno.nombre = oTable.Rows(0).Item("nombre").ToString
             oAlumno.documento = oTable.Rows(0).Item("documento").ToString
             oAlumno.telefono = oTable.Rows(0).Item("telefono").ToString
         Next
