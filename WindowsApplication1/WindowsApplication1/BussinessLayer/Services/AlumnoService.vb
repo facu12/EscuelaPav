0f2667b7c2c92a466aa16fce3155aba8125659e6
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-06 16:28:38 -0300

    Creaciòn servicio Alumno

---
+++ WindowsApplication1/WindowsApplication1/BussinessLayer/Services/AlumnoService.vb
@@ -0,0 +1,10 @@
+﻿Public Class AlumnoService
+    Private oAlumnoDao As AlumnoDAO
+    Public Sub New()
+        oAlumnoDao = New AlumnoDAO()
+    End Sub
+
+    Public Function listarAlumnos() As List(Of Alumno)
+        Return oAlumnoDao.getAlumnos
+    End Function
+End Class
