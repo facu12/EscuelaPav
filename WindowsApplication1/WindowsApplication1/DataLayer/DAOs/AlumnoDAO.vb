6febfdfc600b06817fa41213dc7649d7c471ba9a
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-06 16:29:46 -0300

    Creaciòn DAO Alumno

---
+++ WindowsApplication1/WindowsApplication1/DataLayer/DAOs/AlumnoDAO.vb
@@ -0,0 +1,24 @@
+﻿Public Class AlumnoDAO
+    'Permite obtener todos los alumnos
+    Public Function getAlumnos()
+        Dim oAlumno As Alumno = Nothing
+        Dim oTable As DataTable
+
+        'Armamos el query para ejecutar a la base
+        Dim strSQL As String = "SELECT * from dbo.Alumno"
+        'Delegamos la ejecución del comando SQL al Helper
+        oTable = BDHelper.getDBHelper().ConsultaSQL(strSQL)
+        'Con la tabla devuelta por el Helper creamos un OBJETO Alumno a partir de los datos de fila de la tabla Alumnos
+        For Each row As DataRow In BDHelper.getDBHelper().ConsultaSQL(strSQL).Rows
+            oAlumno = New Alumno()
+            oAlumno.legajo = oTable.Rows(0).Item("legajo").ToString
+            oAlumno.apellido = oTable.Rows(0).Item("apellido").ToString
+            oAlumno.nombre = Convert.ToInt32(oTable.Rows(0).Item("nombre").ToString)
+            oAlumno.documento = oTable.Rows(0).Item("documento").ToString
+            oAlumno.telefono = oTable.Rows(0).Item("telefono").ToString
+        Next
+
+        Return oAlumno
+    End Function
+
+End Class
