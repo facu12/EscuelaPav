3e8b71bf42702cbe0a0ce3c29fc68ec6eb265237
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-06 16:28:15 -0300

    Creaciòn clase entidad Alumno

---
+++ WindowsApplication1/WindowsApplication1/BussinessLayer/Entities/Alumno.vb
@@ -0,0 +1,7 @@
+﻿Public Class Alumno
+    Public Property legajo As Integer
+    Public Property apellido As String
+    Public Property nombre As String
+    Public Property telefono As String
+    Public Property documento As String
+End Class
