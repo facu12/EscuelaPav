d22d02fb6624f0f2d485d028d1809fbe01109913
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-06 18:05:08 -0300

    Registrar Alumno

--- WindowsApplication1/WindowsApplication1/BussinessLayer/Services/AlumnoService.vb
+++ WindowsApplication1/WindowsApplication1/BussinessLayer/Services/AlumnoService.vb
@@ -7,4 +7,9 @@
     Public Function listarAlumnos() As List(Of Alumno)
         Return oAlumnoDao.getAlumnos
     End Function
+
+    Public Function registrarAlumno(ByVal oAlumno As Alumno) As Boolean
+        Return oAlumnoDao.add(oAlumno)
+    End Function
+
 End Class
