2594bb1cd5b0dc3595983b67134d6f51a3597ada
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-11 12:29:08 -0300

    Creacion formulario

---
+++ WindowsApplication1/WindowsApplication1/GUILayer/frmCursos.Designer.vb
@@ -0,0 +1,400 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmCursos
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAño = New System.Windows.Forms.TextBox()
+        Me.txtNivel = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSubnivel = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.cmbProfesor1 = New System.Windows.Forms.ComboBox()
+        Me.cmbProfesor2 = New System.Windows.Forms.ComboBox()
+        Me.cmbProfesor4 = New System.Windows.Forms.ComboBox()
+        Me.cmbProfesor3 = New System.Windows.Forms.ComboBox()
+        Me.cmbProfesor5 = New System.Windows.Forms.ComboBox()
+        Me.cmbMateria1 = New System.Windows.Forms.ComboBox()
+        Me.cmbMateria2 = New System.Windows.Forms.ComboBox()
+        Me.cmbMateria4 = New System.Windows.Forms.ComboBox()
+        Me.cmbMateria3 = New System.Windows.Forms.ComboBox()
+        Me.cmbMateria5 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.codigo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.año = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nivel = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.subnivel = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.turno = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.cmbMateria5)
+        Me.Panel1.Controls.Add(Me.cmbMateria4)
+        Me.Panel1.Controls.Add(Me.cmbMateria3)
+        Me.Panel1.Controls.Add(Me.cmbMateria2)
+        Me.Panel1.Controls.Add(Me.cmbMateria1)
+        Me.Panel1.Controls.Add(Me.cmbProfesor5)
+        Me.Panel1.Controls.Add(Me.cmbProfesor4)
+        Me.Panel1.Controls.Add(Me.cmbProfesor3)
+        Me.Panel1.Controls.Add(Me.cmbProfesor2)
+        Me.Panel1.Controls.Add(Me.cmbProfesor1)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Location = New System.Drawing.Point(21, 133)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(496, 251)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.txtSubnivel)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.txtNivel)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.txtAño)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(21, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(496, 102)
+        Me.Panel2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Año:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtAño
+        '
+        Me.txtAño.Location = New System.Drawing.Point(125, 20)
+        Me.txtAño.Name = "txtAño"
+        Me.txtAño.Size = New System.Drawing.Size(100, 22)
+        Me.txtAño.TabIndex = 1
+        '
+        'txtNivel
+        '
+        Me.txtNivel.Location = New System.Drawing.Point(125, 61)
+        Me.txtNivel.Name = "txtNivel"
+        Me.txtNivel.Size = New System.Drawing.Size(100, 22)
+        Me.txtNivel.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nivel:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtSubnivel
+        '
+        Me.txtSubnivel.Location = New System.Drawing.Point(379, 61)
+        Me.txtSubnivel.Name = "txtSubnivel"
+        Me.txtSubnivel.Size = New System.Drawing.Size(100, 22)
+        Me.txtSubnivel.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(266, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "SubNivel:"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 65)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(70, 17)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Módulo 2:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 24)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Módulo 1:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Módulo 3:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 147)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(70, 17)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Módulo 4:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 188)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(70, 17)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Módulo 5:"
+        '
+        'cmbProfesor1
+        '
+        Me.cmbProfesor1.FormattingEnabled = True
+        Me.cmbProfesor1.Location = New System.Drawing.Point(125, 21)
+        Me.cmbProfesor1.Name = "cmbProfesor1"
+        Me.cmbProfesor1.Size = New System.Drawing.Size(164, 24)
+        Me.cmbProfesor1.TabIndex = 10
+        Me.cmbProfesor1.Text = "Seleccione Profesor..."
+        '
+        'cmbProfesor2
+        '
+        Me.cmbProfesor2.FormattingEnabled = True
+        Me.cmbProfesor2.Location = New System.Drawing.Point(125, 62)
+        Me.cmbProfesor2.Name = "cmbProfesor2"
+        Me.cmbProfesor2.Size = New System.Drawing.Size(164, 24)
+        Me.cmbProfesor2.TabIndex = 11
+        Me.cmbProfesor2.Text = "Seleccione Profesor..."
+        '
+        'cmbProfesor4
+        '
+        Me.cmbProfesor4.FormattingEnabled = True
+        Me.cmbProfesor4.Location = New System.Drawing.Point(125, 144)
+        Me.cmbProfesor4.Name = "cmbProfesor4"
+        Me.cmbProfesor4.Size = New System.Drawing.Size(164, 24)
+        Me.cmbProfesor4.TabIndex = 13
+        Me.cmbProfesor4.Text = "Seleccione Profesor..."
+        '
+        'cmbProfesor3
+        '
+        Me.cmbProfesor3.FormattingEnabled = True
+        Me.cmbProfesor3.Location = New System.Drawing.Point(125, 103)
+        Me.cmbProfesor3.Name = "cmbProfesor3"
+        Me.cmbProfesor3.Size = New System.Drawing.Size(164, 24)
+        Me.cmbProfesor3.TabIndex = 12
+        Me.cmbProfesor3.Text = "Seleccione Profesor..."
+        '
+        'cmbProfesor5
+        '
+        Me.cmbProfesor5.FormattingEnabled = True
+        Me.cmbProfesor5.Location = New System.Drawing.Point(125, 185)
+        Me.cmbProfesor5.Name = "cmbProfesor5"
+        Me.cmbProfesor5.Size = New System.Drawing.Size(164, 24)
+        Me.cmbProfesor5.TabIndex = 14
+        Me.cmbProfesor5.Text = "Seleccione Profesor..."
+        '
+        'cmbMateria1
+        '
+        Me.cmbMateria1.FormattingEnabled = True
+        Me.cmbMateria1.Location = New System.Drawing.Point(315, 21)
+        Me.cmbMateria1.Name = "cmbMateria1"
+        Me.cmbMateria1.Size = New System.Drawing.Size(164, 24)
+        Me.cmbMateria1.TabIndex = 15
+        Me.cmbMateria1.Text = "Seleccione Materia..."
+        '
+        'cmbMateria2
+        '
+        Me.cmbMateria2.FormattingEnabled = True
+        Me.cmbMateria2.Location = New System.Drawing.Point(315, 62)
+        Me.cmbMateria2.Name = "cmbMateria2"
+        Me.cmbMateria2.Size = New System.Drawing.Size(164, 24)
+        Me.cmbMateria2.TabIndex = 16
+        Me.cmbMateria2.Text = "Seleccione Materia..."
+        '
+        'cmbMateria4
+        '
+        Me.cmbMateria4.FormattingEnabled = True
+        Me.cmbMateria4.Location = New System.Drawing.Point(315, 144)
+        Me.cmbMateria4.Name = "cmbMateria4"
+        Me.cmbMateria4.Size = New System.Drawing.Size(164, 24)
+        Me.cmbMateria4.TabIndex = 18
+        Me.cmbMateria4.Text = "Seleccione Materia..."
+        '
+        'cmbMateria3
+        '
+        Me.cmbMateria3.FormattingEnabled = True
+        Me.cmbMateria3.Location = New System.Drawing.Point(315, 103)
+        Me.cmbMateria3.Name = "cmbMateria3"
+        Me.cmbMateria3.Size = New System.Drawing.Size(164, 24)
+        Me.cmbMateria3.TabIndex = 17
+        Me.cmbMateria3.Text = "Seleccione Materia..."
+        '
+        'cmbMateria5
+        '
+        Me.cmbMateria5.FormattingEnabled = True
+        Me.cmbMateria5.Location = New System.Drawing.Point(315, 185)
+        Me.cmbMateria5.Name = "cmbMateria5"
+        Me.cmbMateria5.Size = New System.Drawing.Size(164, 24)
+        Me.cmbMateria5.TabIndex = 19
+        Me.cmbMateria5.Text = "Seleccione Materia..."
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Location = New System.Drawing.Point(537, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(387, 371)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Lista de Cursos"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(379, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(266, 23)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(43, 17)
+        Me.Label9.TabIndex = 6
+        Me.Label9.Text = "Nivel:"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.codigo, Me.año, Me.nivel, Me.subnivel, Me.turno})
+        Me.DataGridView1.Location = New System.Drawing.Point(7, 22)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(423, 343)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'codigo
+        '
+        Me.codigo.HeaderText = "Cod. Curso"
+        Me.codigo.Name = "codigo"
+        '
+        'año
+        '
+        Me.año.HeaderText = "Año"
+        Me.año.Name = "año"
+        '
+        'nivel
+        '
+        Me.nivel.HeaderText = "Nivel"
+        Me.nivel.Name = "nivel"
+        '
+        'subnivel
+        '
+        Me.subnivel.HeaderText = "SubNivel"
+        Me.subnivel.Name = "subnivel"
+        '
+        'turno
+        '
+        Me.turno.HeaderText = "Turno"
+        Me.turno.Name = "turno"
+        '
+        'frmCursos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(979, 431)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "frmCursos"
+        Me.Text = "Cursos"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtAño As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbMateria5 As ComboBox
+    Friend WithEvents cmbMateria4 As ComboBox
+    Friend WithEvents cmbMateria3 As ComboBox
+    Friend WithEvents cmbMateria2 As ComboBox
+    Friend WithEvents cmbMateria1 As ComboBox
+    Friend WithEvents cmbProfesor5 As ComboBox
+    Friend WithEvents cmbProfesor4 As ComboBox
+    Friend WithEvents cmbProfesor3 As ComboBox
+    Friend WithEvents cmbProfesor2 As ComboBox
+    Friend WithEvents cmbProfesor1 As ComboBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtSubnivel As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNivel As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents codigo As DataGridViewTextBoxColumn
+    Friend WithEvents año As DataGridViewTextBoxColumn
+    Friend WithEvents nivel As DataGridViewTextBoxColumn
+    Friend WithEvents subnivel As DataGridViewTextBoxColumn
+    Friend WithEvents turno As DataGridViewTextBoxColumn
+End Class
---
+++ WindowsApplication1/WindowsApplication1/GUILayer/frmCursos.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmCursos
+
+End Class
