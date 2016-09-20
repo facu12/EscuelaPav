73afff764a6e582f0ff77ea04732f0285d5e2114
Nicolas Soria <nico_axe8@hotmail.com>
2016-09-20 16:56:58 -0300

    metodo

---
+++ WindowsApplication1/WindowsApplication1/BussinessLayer/Services/CursoService.vb
@@ -0,0 +1,15 @@
+﻿Public Class CursoService
+    Private oCurso As New CursoDAO
+    Public Sub New()
+        oCurso = New CursoDAO()
+    End Sub
+
+    Public Function listarCursos() As List(Of Curso)
+        Return oCurso.getCursos
+    End Function
+
+    Public Function registrarCurso(curso As Curso) As Boolean
+        Return oCurso.add(curso)
+    End Function
+
+End Class
