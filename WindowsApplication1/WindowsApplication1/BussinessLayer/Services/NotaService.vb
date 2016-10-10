c2db0bc46d7bf94464f3534f79fc94a0377d3408
Nicolas Soria <nico_axe8@hotmail.com>
2016-10-10 19:59:56 -0300

    Nuevo objeto

---
+++ WindowsApplication1/WindowsApplication1/BussinessLayer/Services/NotaService.vb
@@ -0,0 +1,12 @@
+﻿Public Class NotaService
+    Private oNotaDAO As New NotaDAO
+    Public Function getTipoNota() As DataTable
+        Return oNotaDAO.getTipoNota
+    End Function
+
+    Public Function getTrimestres() As DataTable
+        Dim str As String
+        str = "SELECT * FROM Trimestre"
+        Return BDHelper.getDBHelper.ConsultaSQL(str)
+    End Function
+End Class
