0c9799061efaa4b36883200bbf00539f211f53a5
Nicolas Soria <nico_axe8@hotmail.com>
2016-10-12 17:59:15 -0300

    nueva funcion

--- WindowsApplication1/WindowsApplication1/BussinessLayer/Services/NotaService.vb
+++ WindowsApplication1/WindowsApplication1/BussinessLayer/Services/NotaService.vb
@@ -9,4 +9,8 @@
         str = "SELECT * FROM Trimestre"
         Return BDHelper.getDBHelper.ConsultaSQL(str)
     End Function
+
+    Public Function insertarNotas(tabla As DataTable, tnota As Char, trim As Integer, fecha As String, mat As Integer) As Integer
+        Return oNotaDAO.insertarNotas(tabla, tnota, trim, fecha, mat)
+    End Function
 End Class
