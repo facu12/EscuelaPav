3e1ef2ac36db9c28c4d17ba3ad2df5f17f4f1b30
Nicolas Soria <nico_axe8@hotmail.com>
2016-11-08 16:25:13 -0300

    pipo

--- WindowsApplication1/WindowsApplication1/GUILayer/frmEstadisticasNotas.vb
+++ WindowsApplication1/WindowsApplication1/GUILayer/frmEstadisticasNotas.vb
@@ -1,14 +1,14 @@
 ﻿Public Class frmEstadisticasNotas
     Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
-        Dim reporte As New ReporteAsistencias
+        Dim reporte As New DocNotas
         Dim oCursoService As New CursoService
         If (txtAño.Text = "") Then
             MsgBox("Faltan Completar Datos", vbOK, "Datos Incompletos")
         Else
             If (oCursoService.listarCursosAño(txtAño.Text).Count > 0) Then
                 reporte.SetParameterValue("@año", txtAño.Text)
-                Me.rptNotas.ReportSource = reporte
-                Me.rptNotas.Show()
+                rptNotas.ReportSource = reporte
+                rptNotas.Show()
                 rptNotas.Refresh()
             Else
                 MsgBox("No existe el curso buscado. Por favor, seleccione uno diferente", vbOK, "Curso Inexistente")
